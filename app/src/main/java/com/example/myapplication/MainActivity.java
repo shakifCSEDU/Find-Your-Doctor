509591b9917700f85aafcdab83ab32950ca8f7c8
sakif <sakifsahriar@gmail.com>
2022-03-30 23:03:51 +0600

    Set the bottom Navigation View.

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_nav_viewId);
+        NavHostFragment navHostFragment = (NavHostFragment)getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
+        NavController navController = navHostFragment.getNavController();
+        NavigationUI.setupWithNavController(bottomNavigationView,navController);
+
+    }
+}
