249cbee8381c148da5831ef278dc1e000a6c13c0
sakif <sakifsahriar@gmail.com>
2022-04-05 13:05:38 +0600

    setup all bottom navigation icon and actions also configure the appbar.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
 import android.os.Bundle;
@@ -10,16 +11,37 @@ import android.os.Bundle;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-    private BottomNavigationView bottomNavigationView;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_nav_viewId);
-        NavHostFragment navHostFragment = (NavHostFragment)getSupportFragmentManager().findFragmentById(R.id.fragmentContainerView);
-        NavController navController = navHostFragment.getNavController();
+        BottomNavigationView bottomNavigationView =(BottomNavigationView) findViewById(R.id.bottomNavigationViewId);
+
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment_container);
+        navController = navHostFragment.getNavController();
+
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.homeFragment,
+                R.id.historyFragment,
+                R.id.notificationsFragment,
+                R.id.tipsFragment,
+                R.id.profileFragment
+        ).build();
+
+
         NavigationUI.setupWithNavController(bottomNavigationView,navController);
+        NavigationUI.setupActionBarWithNavController(this,navController,appBarConfiguration);
+
+
 
     }
+    @Override
+    public boolean onSupportNavigateUp() {
+        return super.onSupportNavigateUp() || navController.navigateUp();
+    }
+
+
 }
--- app/src/main/java/com/example/myapplication/TipsFragment.java
+++ app/src/main/java/com/example/myapplication/TipsFragment.java
@@ -10,10 +10,10 @@ import android.view.ViewGroup;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link NewsFragment#newInstance} factory method to
+ * Use the {@link TipsFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class NewsFragment extends Fragment {
+public class TipsFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +24,7 @@ public class NewsFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public NewsFragment() {
+    public TipsFragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +34,11 @@ public class NewsFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment NewsFragment.
+     * @return A new instance of fragment TipsFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static NewsFragment newInstance(String param1, String param2) {
-        NewsFragment fragment = new NewsFragment();
+    public static TipsFragment newInstance(String param1, String param2) {
+        TipsFragment fragment = new TipsFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +59,6 @@ public class NewsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news, container, false);
+        return inflater.inflate(R.layout.fragment_tips, container, false);
     }
 }
