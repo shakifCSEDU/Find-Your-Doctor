5dba24f70fa823517f8e283b608dd4693f58f147
sakif <sakifsahriar@gmail.com>
2022-04-06 01:35:37 +0600

    add shimmerRecyclerView in Home

--- app/src/main/java/com/example/myapplication/HomeFragment.java
+++ app/src/main/java/com/example/myapplication/HomeFragment.java
@@ -8,13 +8,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.facebook.shimmer.ShimmerFrameLayout;
+
 
 public class HomeFragment extends Fragment {
-
+    private ShimmerFrameLayout shimmerFrameLayout;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.fragment_home, container, false);
+
+       View view = inflater.inflate(R.layout.fragment_home, container, false);
+        shimmerFrameLayout = (ShimmerFrameLayout)view.findViewById(R.id.shimmerId);
+        shimmerFrameLayout.startShimmer();
+
+
+
+        return view;
     }
 }
