dbd2569c33a074a8a44938ef08717fa2bfb9e1fd
Prodipta0109 <prodiptasenamartya1971@gmail.com>
2022-04-09 20:39:24 +0600

    Doctor Registration Complete

--- app/src/main/java/com/example/myapplication/DoctorRegistrationActivity.java
+++ app/src/main/java/com/example/myapplication/DoctorRegistrationActivity.java
@@ -3,12 +3,25 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class DoctorRegistrationActivity extends AppCompatActivity {
+
+    private AutoCompleteTextView regionAutoCompleteTextView, doctorSpecialityAutoCompleteTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor_registration);
+
+        String[] Dhaka_Region = getResources().getStringArray(R.array.Dhaka_region);
+        ArrayAdapter<String> arrayAdapter1 = new ArrayAdapter<>(this, R.layout.dropdown_item, Dhaka_Region);
+        regionAutoCompleteTextView.setAdapter(arrayAdapter1);
+
+        String[] Doctor_Speciality = getResources().getStringArray(R.array.Doctor_Speciality);
+        ArrayAdapter<String> arrayAdapter2 = new ArrayAdapter<>(this, R.layout.dropdown_item, Doctor_Speciality);
+        regionAutoCompleteTextView.setAdapter(arrayAdapter2);
+
     }
 }
--- app/src/main/java/com/example/myapplication/PatientRegistrationActivity.java
+++ app/src/main/java/com/example/myapplication/PatientRegistrationActivity.java
@@ -3,12 +3,20 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 
 public class PatientRegistrationActivity extends AppCompatActivity {
+
+    private AutoCompleteTextView districtAutoCompleteView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_registration);
+
+        String[] District = getResources().getStringArray(R.array.District);
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this, R.layout.dropdown_item, District);
+        districtAutoCompleteView.setAdapter(arrayAdapter);
     }
 }
